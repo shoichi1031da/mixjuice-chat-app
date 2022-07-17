e787457c504ddcbe74002c15341db67522775251
Shoichi Nakamura <41470674+shoichi1031da@users.noreply.github.com>
2022-07-18 01:12:02 +0900

    add cors2

--- server.js
+++ server.js
@@ -7,20 +7,15 @@ const { receiveMessageOnPort } = require("worker_threads");
 const server = http.createServer(app);
 const io = require("socket.io")(server);
 
-const allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Content-Type, Authorization, access_token'
-    )
-  
-    // intercept OPTIONS method
-    if ('OPTIONS' === req.method) res.send(200);
-    else next();
-  }
-
-  app.use(allowCrossDomain);
+const cors = require('cors');
+app.use(cors());
+const corsOption = {
+    origin: [
+      "http://https://fukuno.jig.jp "
+    ],
+    credentials: true,
+  };
+  app.use(cors(corsOption));
 
 
 const PORT = process.env.PORT || 3000;
