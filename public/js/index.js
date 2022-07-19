742d9f49f46bf6bd6eb0335ad55a5f712dbd742c
Shoichi Nakamura <41470674+shoichi1031da@users.noreply.github.com>
2022-07-20 07:13:26 +0900

    DOM test1

--- public/js/index.js
+++ public/js/index.js
@@ -12,6 +12,26 @@ let nowDate = () => {
 }
 
 
+let client_w = document.getElementById('notes').clientWidth;
+console.log(client_w);
+
+let notes = document.getElementById('notes');
+let note = document.getElementById('note');
+let src = document.getElementById('src');
+let line = document.getElementById('line');
+if(client_w > 415){
+  
+  notes.style.flexWrap = "none";
+  note.style.flexGrow = "none"
+  src.style.width = "0%";
+  src.style.flexGrow = "1";
+  src.style.flexBasis = "0";
+  line.style.width = "0%";
+  line.style.margin = "5px 10px 5px 0";
+}
+
+
+
 //全クライアントのブラウザに表示
 let socket = io();
 socket.on("chat message", (MSG,ICON) => {
